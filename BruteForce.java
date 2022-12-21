3c2677db57ac6761a46722828824d9b33cf56326
Brahim Kellou <brahimkellou@outlook.com>
2022-12-21 17:40:39 +0100

    fix: handle edge case for brute force approach

--- BruteForce.java
+++ BruteForce.java
@@ -1,7 +1,15 @@
 public class BruteForce {
     public static int findSecondLargest(int[] arr) {
-        int largest = Integer.MIN_VALUE;
-        int secondLargest = Integer.MAX_VALUE;
+
+        // The size of the array should be at least equal to 2.
+
+        // Initialize the largest with the max between the first element and the second
+        // element of the array.
+        int largest = Math.max(arr[0], arr[1]);
+
+        // Initialize the secondLargest with the min between the first element and the
+        // second element of the array.
+        int secondLargest = Math.min(arr[0], arr[1]);
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > largest) {
